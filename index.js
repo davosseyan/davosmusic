b8561f8e7d0c5952237f8165b28904896f66c025
hede seyan <78200081+davosseyan@users.noreply.github.com>
2021-04-06 15:08:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('NzI4MzY4OTE3NDg3MDI2MjI4.Xv5Yag.1MRJv0wd5ih4U_4xdfIx1ImUTJo');
+client.login('NzI4MzY4OTE3NDg3MDI2MjI4.Xv5Yag.46GeOuqidHdmA6cg4moX1w3-R20');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
