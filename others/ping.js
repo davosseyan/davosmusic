ef34287ce2f6ab178f90418efb263eb5f296041d
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-04-09 00:02:30 +0300

    Update ping.js

--- others/ping.js
+++ others/ping.js
@@ -13,7 +13,7 @@ module.exports = {
   edesc: "Type this command to see how fast the Bot can response to your messages / commands inputs!",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("<:emoji_74:815251307635015731>");
+    message.react("✅");
     //send the Ping embed
     message.reply(new MessageEmbed().setColor("YELLOW").setTitle("<:emoji_74:815251307635015731> `" + client.ws.ping + "ms`"));
   }
